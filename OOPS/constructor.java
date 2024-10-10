23a5a247b85c6a5e923178bad46a92da333bb571
Mintu <km4812998@example.com>
2024-10-10 22:55:22 +0530

    const uodated

--- OOPS/constructor.java
+++ OOPS/constructor.java
@@ -1,15 +1,25 @@
 public class constructor {
     public static void main(String args[]){
-        Student s1=new Student("Mintu Kumar");
+        Student s1=new Student();
         System.out.println(s1.name);
+        Student s2= new Student("Mintu Kumar");
+        System.out.println(s2.name);
+        Student s3=new Student(123);
+        System.out.println(s3.roll);
     }
 }
 
 class Student{
     String name;
+    int roll;
 
-    Student(String name){
+    Student(){
         System.out.println("Constructor called..");
+    }
+    Student(String name){
         this.name=name;
+    }
+    Student(int roll){
+        this.roll=roll;
     }
 }
