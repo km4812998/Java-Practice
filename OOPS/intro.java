9c629b85b6e6f5bc9785f0135099f3fc09f29173
Mintu <km4812998@example.com>
2024-10-10 13:46:20 +0530

    intro for getters (get) and setters(set)

--- OOPS/intro.java
+++ OOPS/intro.java
@@ -2,32 +2,40 @@ public class intro{
     public static void main(String[] args){
         Pen p1=new Pen();
         p1.setColor("Blue");
-        System.out.println(p1.color);
+        System.out.println("Pen color: " + p1.getColor());
         p1.setTip(5);
-        System.out.println(p1.tip);
+        System.out.println("Tip Size: " + p1.gettipSize());
 
-        BankAccount myAcc=new BankAccount();
-        myAcc.username="KumarMintu";
-        System.out.println(myAcc.username);
-        myAcc.setPassword("abcdefgh");
+        // BankAccount myAcc=new BankAccount();
+        // myAcc.username="KumarMintu";
+        // System.out.println("userName: " +  myAcc.username);
+        // myAcc.setPassword("abcdefgh");
         // System.out.println(myAcc.setPassword());
     }
 }
 class Pen{
-    String color;
-    int tip;
+    private String color;
+    private int tipSize;
+
+    String getColor(){
+        return this.color;
+    }
+    int gettipSize(){
+        return this.tipSize;
+    }
+
     void setColor(String newColor){
         color=newColor;
     }
     void setTip(int newTip){
-        tip=newTip;
+        tipSize=newTip;
     }
 }
-class BankAccount{
-    public String username;
-    private String password;
+// class BankAccount{
+//     public String username;
+//     private String password;
 
-    void setPassword(String pwd){
-        password=pwd;
-    }
-}
+//     void setPassword(String pwd){
+//         password=pwd;
+//     }
+// }
