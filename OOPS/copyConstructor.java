cd73fcaa40f4fc7585f35ae6c575739d9fdc328d
Mintu <km4812998@example.com>
2024-10-13 19:43:21 +0530

    constructor updated

--- OOPS/copyConstructor.java
+++ OOPS/copyConstructor.java
@@ -1,31 +1,31 @@
-public class copyConstructor {
-    public static void main(String[] args) {
-        Student s1= new Student();
-        s1.name="Mintu Kumar";
-        s1.roll=107;
-        s1.password="aabcdd";
+// public class copyConstructor {
+//     public static void main(String[] args) {
+//         Student s1= new Student();
+//         s1.name="Mintu Kumar";
+//         s1.roll=107;
+//         s1.password="aabcdd";
 
-        Student s2=new Student(s1);
-        s2.password="defg";
-    }
-}
+//         Student s2=new Student(s1);
+//         s2.password="defg";
+//     }
+// }
 
-class Student{
-    String name;
-    int roll;
-    String password;
+// class Student{
+//     String name;
+//     int roll;
+//     String password;
 
-    Student(Student s1){
-        this.name=s1.name;
-        this.roll=s1.roll;
-    }
-    Student(){
-        System.out.println("Constructor called..");
-    }
-    Student(String name){
-        this.name=name;
-    }
-    Student(int roll){
-        this.roll=roll;
-    }
-}
+//     Student(Student s1){
+//         this.name=s1.name;
+//         this.roll=s1.roll;
+//     }
+//     Student(){
+//         System.out.println("Constructor called..");
+//     }
+//     Student(String name){
+//         this.name=name;
+//     }
+//     Student(int roll){
+//         this.roll=roll;
+//     }
+// }
